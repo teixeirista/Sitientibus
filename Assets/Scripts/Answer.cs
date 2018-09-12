f6e2a6405ae5f0dc9e8509cd949ea11998f573eb
teixeirista <teixeirista@gmail.com>
2018-09-12 13:21:23 -0300

    first puzzle

---
+++ Assets/Scripts/Answer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Answer : MonoBehaviour {
+
+	InputField iField;
+	public GameObject wrong;
+
+	void Start(){
+		iField = GetComponent<InputField>();
+	}
+     
+    public void VerifyAnswer()
+    {
+		if(iField.text == "anarquia e sexo" || iField.text == "Anarquia e Sexo" || iField.text == "Anarquia e sexo") {
+			wrong.SetActive(false);
+		} else
+		{
+			wrong.SetActive(true);
+		}
+    }
+
+}
