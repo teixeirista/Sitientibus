df8ba594f95a959997b348a194557fcad6564bfc
teixeirista <teixeirista@gmail.com>
2018-10-15 23:36:54 -0300

    outros enigmas adicionados

--- Assets/Fonts/FontScript.cs
+++ Assets/Fonts/FontScript.cs
@@ -10,6 +10,7 @@ public class FontScript : MonoBehaviour {
 	InputField iField;
 	string answer;
 	public GameObject wrong;
+	public GameObject almost;
 
 	void Start(){
 		iField = GetComponent<InputField>();
@@ -22,12 +23,27 @@ public class FontScript : MonoBehaviour {
 
 		if(answer.Contains("feira de santana")) {
 			SceneManager.LoadScene(2);
-		} 
-		else if(answer.Contains("cidade nova") && answer.Contains("jomafa") && answer.Contains("viveiros")){
+		}
+		else if(answer.Contains("trem") && answer.Contains("tomba")) {
 			SceneManager.LoadScene(3);
 		}
-		else if(answer.Contains("festa de santana")) {
+		else if(answer.Contains("cidade nova") && answer.Contains("jomafa") && answer.Contains("viveiros")) {
+			SceneManager.LoadScene(3);
+		}
+		else if(answer.Contains("festa") && answer.Contains("de") && answer.Contains("santana")) {
 			SceneManager.LoadScene(4);
+		}
+		else if(answer.Contains("1009")) {
+			SceneManager.LoadScene(5);
+		}
+		else if(answer.Contains("ovni")) {
+			SceneManager.LoadScene(5);
+		}
+		else if(answer.Contains("princesa do sertão")) {
+			SceneManager.LoadScene(5);
+		}
+		else if(answer.Contains("todos os caminhos")) {
+			SceneManager.LoadScene(5);
 		}
 		else {
 			wrong.SetActive(true);
